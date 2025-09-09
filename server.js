1e3ae638af98a1ab6b01306d70d2dccacfbd5784
cristaleeh <cristaleeh12@gmail.com>
2025-09-08 22:04:31 -0500

    Install dependencies

---
+++ server.js
@@ -0,0 +1,27 @@
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+
+
+
+
+
+
+const port = process.env.PORT ? process.env.PORT : '3000';
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected');
+});
+
+
+// Middleware to parse URL-encoded data from forms
+app.use(express.urlencoded({ extended: false }));
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride("_method"));
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
